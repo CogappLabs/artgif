1f16a3dbc584d2851a0838f5314cc1ccd4512a71
Matt Chapman <mattc@cogapp.com>
2021-02-05 15:31:47 +0000

    Set active artwork from lightbox

--- src/Components/FilmstripPanel/Lightbox/Lightbox.tsx
+++ src/Components/FilmstripPanel/Lightbox/Lightbox.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { setActiveArtworkAction } from '../../../Store/artworks';
 import { ImageDescriptor } from '../../../Store/artworks/artworks.types';
 import './Lightbox.css';
 
@@ -9,10 +11,14 @@ export interface LightboxProps {
 export type LightboxFC = React.FC<LightboxProps>;
 
 export const Lightbox: LightboxFC = ({ artworks }) => {
+  const dispatch = useDispatch();
+
+  const imageClickHandler = (artwork: ImageDescriptor) => dispatch(setActiveArtworkAction(artwork));
+
   return (
     <div className="lightbox">
       {artworks.map((artwork) => {
-        return <img key={artwork.uuid} src={artwork.imageUrl} alt="" />;
+        return <img key={artwork.uuid} onClick={() => imageClickHandler(artwork)} src={artwork.imageUrl} alt="" />;
       })}
     </div>
   );
