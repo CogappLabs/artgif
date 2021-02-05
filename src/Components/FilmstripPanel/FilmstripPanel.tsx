dc9f516ab6319bd4134daee076ef210f92516abe
Matt Chapman <mattc@cogapp.com>
2021-02-05 00:08:07 +0000

    Add basic panel container components

---
+++ src/Components/FilmstripPanel/FilmstripPanel.tsx
@@ -0,0 +1,7 @@
+import { FunctionComponent } from 'react';
+
+export interface FilmstripPanelProps {}
+
+export const FilmstripPanel: FunctionComponent<FilmstripPanelProps> = () => {
+  return <h1>Filmstrip area</h1>;
+};
