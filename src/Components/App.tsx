37d840094bde2ddc86594cc44718eba3fc90775a
Matt Chapman <mattc@cogapp.com>
2021-02-05 11:18:51 +0000

    Add empty store

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -7,28 +7,32 @@ import { FilmstripPanel } from './FilmstripPanel';
 import { EditorPanel } from './EditorPanel';
 import { OutputPanel } from './OutputPanel';
 import { Panel } from './Panel';
+import { Provider as ReduxProvider } from 'react-redux';
+import { store } from '../Store/store';
 
 export const App: FunctionComponent = () => {
   return (
-    <div className="App">
-      <Header />
-      <main className="layout">
-        <div className="panel-grid">
-          <Panel title="Artworks" type="artworks">
-            <ArtworkPanel />
-          </Panel>
-          <Panel title="Filmstrip" type="filmstrip">
-            <FilmstripPanel />
-          </Panel>
-          <Panel title="Editor" type="editor">
-            <EditorPanel />
-          </Panel>
-          <Panel title="Output" type="output">
-            <OutputPanel />
-          </Panel>
-        </div>
-      </main>
-      <Footer />
-    </div>
+    <ReduxProvider store={store}>
+      <div className="App">
+        <Header />
+        <main className="layout">
+          <div className="panel-grid">
+            <Panel title="Artworks" type="artworks">
+              <ArtworkPanel />
+            </Panel>
+            <Panel title="Filmstrip" type="filmstrip">
+              <FilmstripPanel />
+            </Panel>
+            <Panel title="Editor" type="editor">
+              <EditorPanel />
+            </Panel>
+            <Panel title="Output" type="output">
+              <OutputPanel />
+            </Panel>
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </ReduxProvider>
   );
 };
