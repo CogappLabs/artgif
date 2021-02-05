1aec797f398332f844f4e33e7c2bf1fddd0d5c43
Matt Chapman <mattc@cogapp.com>
2021-02-05 14:50:17 +0000

    Add artwork actions

--- src/Store/darkmode/darkmode.actions.ts
+++ src/Store/darkmode/darkmode.actions.ts
@@ -1,8 +1,6 @@
 import { DarkModeActionTypes, TOGGLE_DARK_MODE } from './darkmode.types';
 
-export const toggleDarkModeAction = (newStatus: boolean): DarkModeActionTypes => {
-  return {
-    type: TOGGLE_DARK_MODE,
-    newStatus,
-  };
-};
+export const toggleDarkModeAction = (newStatus: boolean): DarkModeActionTypes => ({
+  type: TOGGLE_DARK_MODE,
+  newStatus,
+});
