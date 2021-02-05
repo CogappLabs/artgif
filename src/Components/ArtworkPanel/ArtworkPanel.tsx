b419128011934f1baa9c67482f10159a0800762c
Tristan Roddis <tristanr@cogapp.com>
2021-02-05 14:44:21 +0000

    added randomness

--- src/Components/ArtworkPanel/ArtworkPanel.tsx
+++ src/Components/ArtworkPanel/ArtworkPanel.tsx
@@ -11,11 +11,21 @@ export const ArtworkPanel: FunctionComponent<ArtworkPanelProps> = () => {
     }, []);
 
     const getImages = async () => {
-        // TODO add randomness, topics, search
-        let url = 'https://api.artic.edu/api/v1/artworks/search?query[term][is_public_domain]=true&size=20&page=2&fields=id,title,image_id';
+        let perPage = 40;
+        // TODO query the exact number, but for now hardcode public domain images as 55k
+        // let max = Math.floor(55000 / perPage);
+        // Nope. AIC doesn't like page and size values that multiply to over 10000. E.g.
+        // https://api.artic.edu/api/v1/artworks/search?query[term][is_public_domain]=true&size=20&page=501&fields=id,title,image_id
+        let max = 1000/ perPage;
+
+        let pageNo = Math.floor(Math.random() * max);
+        // TODO add topics, search
+
+        let url = 'https://api.artic.edu/api/v1/artworks/search?query[term][is_public_domain]=true&size=' + perPage + '&page=' + pageNo + '&fields=id,title,image_id';
         const response = await fetch(url)
         let data = await response.json();
-        console.log('API data', data)
+        console.log('Geting ' + perPage + ' results at page ' +pageNo)
+        // console.log('API data', data)
         setImages(data.data)
     };
 
