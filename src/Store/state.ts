52b2a5349a865975cec5ff80a59185db395fbb0b
Matt Chapman <mattc@cogapp.com>
2021-02-05 14:39:50 +0000

    Add typed selector

--- src/Store/state.ts
+++ src/Store/state.ts
@@ -1,5 +1,8 @@
+import { useSelector, TypedUseSelectorHook } from 'react-redux';
 import { DarkModeState } from './darkmode';
 
 export interface RootState {
   darkmode: DarkModeState;
 }
+
+export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
