c6559da845a9e31402650c80c1707003588c610e
Matt Chapman <mattc@cogapp.com>
2021-03-02 10:00:24 +0000

    Prevent Gif re-rendering when it has no need

--- src/Components/OutputPanel/GifShot/GifShot.tsx
+++ src/Components/OutputPanel/GifShot/GifShot.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import { ImageDescriptor } from '../../../Store/artworks';
 import gifshot from 'gifshot';
 import { useState } from 'react';
@@ -15,22 +15,24 @@ export interface GifShotProps {
 
 export type GifShotFC = React.FC<GifShotProps>;
 
-export const GifShot: GifShotFC = React.memo(({ images, ...props }) => {
+export const GifShot: GifShotFC = React.memo(({ images = [], ...props }) => {
   const [imageString, setImageString] = useState<string | undefined>(undefined);
 
-  gifshot.createGIF(
-    {
-      images: images.map((image) => image.imageUrl),
-      width: props.width ?? 200,
-      height: props.height ?? 200,
-      frameDuration: props.frameDuration ?? 5,
-    },
-    (params: gifshot.GifShotCallbackParam) => {
-      if (!params.error && params.image) {
-        setImageString(params.image);
+  useLayoutEffect(() => {
+    gifshot.createGIF(
+      {
+        images: images.map((image) => image.imageUrl),
+        width: props.width ?? 200,
+        height: props.height ?? 200,
+        frameDuration: props.frameDuration ?? 5,
+      },
+      (params: gifshot.GifShotCallbackParam) => {
+        if (!params.error && params.image) {
+          setImageString(params.image);
+        }
       }
-    }
-  );
+    );
+  }, [images, props]);
 
   return (
     <div className="gifshot">
